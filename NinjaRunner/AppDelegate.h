ffbf9b1f57d1ed5fdb2face5866f75eb51868080
Gabriela Angelova <gabriela.angelova@fdiba.tu-sofia.bg>
2014-11-02 16:31:52 +0200

    .init

---
+++ NinjaRunner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NinjaRunner
+//
+//  Created by Gabriela Angelova on 11/2/14.
+//  Copyright (c) 2014 telerik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NinjaRunner/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  NinjaRunner
+//
+
+//  Copyright (c) 2014 telerik. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
